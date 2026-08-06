6e9ebab3593fe2009a42476c3bc5e31740e5d3a6
Srinivasan Irulappan <srisan619@gmail.com>
2026-08-06 17:53:31 +0530

    Building blogs - FastAPI

---
+++ backend/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from core.config import settings
+from db.session import engine
+from db.base_class import Base
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+app = start_application()
+
+@app.get("/")
+def home():
+    return {"message": "welcome to the world"}
